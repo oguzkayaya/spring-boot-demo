91faa1ad541350569a9b9f7b9d83780179ce4912
oguz <oguz@DESKTOP-AVHTTKE>
2020-05-02 14:16:36 +0300

    play around, add some stuff

--- webdemo/src/main/java/com/example/webdemo/controller/LoginController.java
+++ webdemo/src/main/java/com/example/webdemo/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -13,6 +14,16 @@ public class LoginController {
 		model.put("name", name);
 		System.out.println("name is " + name);
 		return "login";
+	}
 
+	@RequestMapping("/about")
+	@ResponseBody
+	public String aboutMessage() {
+		return "hello from about";
+	}
+	
+	@RequestMapping("/training")
+	public String trainingPage() {
+		return "train";
 	}
 }

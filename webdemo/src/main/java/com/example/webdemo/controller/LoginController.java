39d73a44bb62cd6d58e69321e0d92d890bc35681
oguz <oguz@DESKTOP-AVHTTKE>
2020-05-03 12:42:21 +0300

    form added, parameter received from post request

--- webdemo/src/main/java/com/example/webdemo/controller/LoginController.java
+++ webdemo/src/main/java/com/example/webdemo/controller/LoginController.java
@@ -3,16 +3,20 @@ package com.example.webdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
-	public String loginMessage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		System.out.println("name is " + name);
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage(ModelMap model) {
 		return "login";
-
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String welcomePage(ModelMap model, @RequestParam String name) {
+		model.put("name", name);
+		return "welcome";
 	}
 }
